f0485388ec48e32738923b3633dacff154bccd9e
ka91969 <ka91969@gmail.com>
2026-05-30 21:33:50 +0300

    Add files via upload

---
+++ send_mail.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to      = "your@email.com";
+    $name    = htmlspecialchars($_POST["name"]);
+    $from    = htmlspecialchars($_POST["email"]);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $headers = "From: $from\r\nReply-To: $from";
+    mail($to, $subject, "From: $name\n\n$message", $headers);
+
+    header("Location: contact.html?success=1");
+}
+?>
